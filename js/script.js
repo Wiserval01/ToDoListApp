26169e4427f428f234d30da264285c17dcefe4ce
Rustam Guliyev <rguliyev93@gmail.com>
2023-07-18 17:01:10 +0400

    All_files_updated

--- js/script.js
+++ js/script.js
@@ -1,87 +1,55 @@
-(function() {
-	'use strict';
-	var tasker = {
-		init: function() {
-			this.cacheDom();
-			this.bindEvents();
-			this.evalTasklist();
-		},
-		cacheDom: function() {
-			this.taskInput = document.getElementById("input-task");
-			this.addBtn = document.getElementById("add-task");
-			this.tasklist = document.getElementById("tasks");
-			this.tasklistChildren = this.tasklist.children;
-			this.errorMessage = document.getElementById("error");
-		},
-		bindEvents: function() {
-			this.addBtn.onclick = this.addTask.bind(this);
-			this.taskInput.onkeypress = this.enterKey.bind(this);
-		},
-		evalTasklist: function() {
-			var i, chkBox, delBtn;
-			for (i = 0; i < this.tasklistChildren.length; i += 1) {
- 
-				chkBox = this.tasklistChildren[i].getElementsByTagName("input")[0];
-				chkBox.onclick = this.completeTask.bind(this, this.tasklistChildren[i], chkBox);
- 
-				delBtn = this.tasklistChildren[i].getElementsByTagName("button")[0];
-				delBtn.onclick = this.delTask.bind(this, i);
-			}
-		},
-		render: function() {
-			var taskLi, taskChkbx, taskVal, taskBtn, taskTrsh;
-			taskLi = document.createElement("li");
-			taskLi.setAttribute("class", "task");
-			taskChkbx = document.createElement("input");
-			taskChkbx.setAttribute("type", "checkbox");
-			taskVal = document.createTextNode(this.taskInput.value);
-			taskBtn = document.createElement("button");
-			taskTrsh = document.createElement("i");
-			taskTrsh.setAttribute("class", "fa fa-trash");
-			taskBtn.appendChild(taskTrsh);
- 
-			taskLi.appendChild(taskChkbx);
-			taskLi.appendChild(taskVal);
-			taskLi.appendChild(taskBtn);
- 
-			this.tasklist.appendChild(taskLi);
- 
-		},
-		completeTask: function(i, chkBox) {
-			if (chkBox.checked) {
-				i.className = "task completed";
-			} else {
-				this.incompleteTask(i);
-			}
-		},
-		incompleteTask: function(i) {
-			i.className = "task";
-		},
-		enterKey: function(event) {
-			if (event.keyCode === 13 || event.which === 13) {
-				this.addTask();
-			}
-		},
-		addTask: function() {
-			var value = this.taskInput.value;
-			this.errorMessage.style.display = "none";
- 
-			if (value === "") {
-				this.error();
-			} else {
-				this.render();
-				this.taskInput.value = "";
-				this.evalTasklist();
-			}
-		},
-		delTask: function(i) {
-			this.tasklist.children[i].remove();
-			this.evalTasklist();
-		},
-		error: function() {
-			this.errorMessage.style.display = "block";
-		}
-	};
- 
-	tasker.init();
-}());
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
